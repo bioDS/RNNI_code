f46a943ef2a866859ed4db471460c2b3d0b31cab
lenacoll <lena.collienne@gmail.com>
2019-12-02 12:11:06 +1300

    change csort to new version of paper

--- caterpillar.py
+++ caterpillar.py
@@ -3,7 +3,7 @@ __author__ = "Lena Collienne"
 #functions on caterpillar trees, specifically computing distances and shortest paths between caterpillar trees
 
 from tree_generator import *
-
+import copy
 
 def is_caterpillar(tree):
     # Returns True if the given tree is a caterpillar tree and False otherwise
@@ -19,100 +19,50 @@ def is_caterpillar(tree):
     return True
 
 
-
-def rank_list(tree):
-    # Returns a list containing for each taxon its rank and the rank of its parent (at position 'P' + taxon)
+def caterpillar_list(tree):
+    # Returns a list representing a caterpillar tree as introduced in Caterpillar Sort (Section  1.2)
     n = get_num_taxa(tree)
     j = 1
-    rank = {}
-    for i in range(1, n+1):
-        rank[str(i)] = 0
-        rank['P' + str(i)] = 0
-    for i in range (len(tree)):
-        if len(tree[i]) == 1:
-            for x in tree[i]:
-                rank[x] = j
-        if len(tree[i]) > 1:
-            for x in tree[i]:
-                if rank['P' + x] == 0:
-                    rank['P' + x] = j
-        j = j + 1
-    return rank
+    clist = []
+    clist.append(int(min(tree[0])))
+    clist.append(int(max(tree[0])))
+    # Add all remaining taxa to the list
+    for i in range(1,len(tree)):
+        clist.append(int((tree[i].difference(tree[i-1])).pop()))
+    return clist
 
+
+def caterpillar_list_to_tree(clist):
+    tree = []
+    for i in range(len(clist)-1): #Add cherry taxa to all sets
+        tree.append(set([clist[0], clist[1]]))
+    for i in range(2,len(clist)):
+        for j in range(i-1, len(tree)):
+            tree[j].add(clist[i])
+    return(tree)
 
 
 def shortest_caterpillar_path(tree1, tree2):
-    # Returns a shortest caterpillar path between two caterpillar trees if all taxa have ranks smaller than all internal nodes
-    path = [tree1]
-    rank2 = rank_list(tree2)
+    # Returns a shortest caterpillar path between two caterpillar trees (ultrametric)
     if is_caterpillar(tree1) and is_caterpillar(tree2) and get_num_taxa(tree1) == get_num_taxa(tree2):
-        current_tree = tree1
-        for j in range(len(current_tree), 1, -1):
-            for i in range (0, j-1):
-                # Special case: taxa
-                if len(current_tree[i]) == 1:
-                    taxon1 = current_tree[i].difference({}).pop()
-                    # Swap ranks of two taxa if necessary
-                    if len(current_tree[i+1]) == 1:
-                        taxon2 = current_tree[i+1].difference({}).pop()
-                        if (rank2[taxon1] > rank2[taxon2]) and swap_ranks(current_tree, i) != None:
-                            current_tree = swap_ranks(current_tree, i)[0]
-                            path = path + [current_tree]
-
-                    # Swap ranks of taxon and internal node with higher rank (not parent)
-                    elif taxon1 not in current_tree[i+1]:
-                        # Find taxon which is child of current_tree[i+1]
-                        k = i-1
-                        while len(current_tree[k].intersection(current_tree[i+1])) == 0:
-                            k = k - 1
-                        taxon2 = current_tree[i+1].difference(current_tree[k]).pop()
-                        if rank2[taxon1] > rank2['P' + taxon2]:
-                            current_tree = swap_ranks(current_tree, i)[0]
-                            path = path + [current_tree]
-
-                # Rank swap of internal node and taxon where taxon has the higher rank
-                elif len(current_tree[i]) > 1 and len(current_tree[i+1]) == 1:
-                    taxon1 = current_tree[i+1].difference({}).pop()
-                    k = i + 1
-                    while (current_tree[k] == 1):
-                        k = k + 1
-                    taxon2 = current_tree[i].difference(current_tree[k]).pop()
-                    if (rank2['P' + taxon2] > rank2[taxon1]):
-                        current_tree = swap_ranks(current_tree, i)[0]
-                        path = path + [current_tree]
-
-                # Special case: cherry - two taxa with equal ranks
-                elif len(current_tree[i]) == 2:
-                    cherry = current_tree[i].difference({})
-                    taxon1 = cherry.pop()
-                    taxon2 = cherry.pop()
-                    taxon3 = current_tree[i+1].difference(current_tree[i]).pop()
-                    aux_trees = make_nni(current_tree, i)
-                    aux_rank = rank_list(aux_trees[0])
-                    if rank2['P' + taxon1] > rank2['P' + taxon3] and rank2['P' + taxon1] > rank2['P' + taxon2] and make_nni(current_tree, i) != None:
-                        if (aux_rank['P' + taxon1] > aux_rank['P' + taxon2]):
-                            current_tree = aux_trees[0]
-                        else:
-                            current_tree = aux_trees[1]
-                        path = path + [current_tree]
-                    elif rank2['P' + taxon2] > rank2['P' + taxon3] and rank2['P' + taxon1] < rank2['P' + taxon2] and make_nni(current_tree, i) != None:
-                        if (aux_rank['P' + taxon1] < aux_rank['P' + taxon2]):
-                            current_tree = aux_trees[0]
-                        else:
-                            current_tree = aux_trees[1]
-                        path = path + [current_tree]
-
-                # Regular case: nni move on internal nodes
-                elif len(current_tree[i]) > 2:
-                    taxon1 = current_tree[i].difference(current_tree[i-1]).pop()
-                    taxon2 = current_tree[i+1].difference(current_tree[i]).pop()
-                    if rank2['P' + taxon1] > rank2['P' + taxon2] and make_nni(current_tree, i) != None:
-                        current_tree = make_nni(current_tree, i)[0]
-                        path = path + [current_tree]
+        clist1 = caterpillar_list(tree1)
+        clist2 = caterpillar_list(tree2)
+        current_tree = clist1
+        n = len(clist1)
+        cpath = [copy.copy(current_tree)] # computed path with trees in caterpillar list representation
+        for j in range(n-1, 1, -1):
+            for i in range (0, j):
+                if current_tree[i] == clist2[j]:
+                    x = current_tree[i]
+                    current_tree[i] = current_tree[i+1]
+                    current_tree[i+1] = x
+                    cpath.append(copy.copy(current_tree))
+        path = [] # Output path with trees in cluster representation
+        for i in cpath:
+            path.append(caterpillar_list_to_tree(i))
         return path
     else:
         print('Input trees are not caterpillars with equal number of taxa')
-
 
 
 def caterpillar_distance(tree1, tree2):
--- caterpillar_path.py
+++ caterpillar_path.py
@@ -3,7 +3,6 @@ __author__ = "Lena Collienne"
 #Computes caterpillar path and distance between two caterpillar trees that need to be given in a txt files in cluster representation [{},{},..] (two rows, each one caterpillar tree)
 
 from caterpillar import *
-from uRNNI_graph import *
 
 def main():
     tree_list = read_trees_from_file()
@@ -14,18 +13,8 @@ def main():
     if is_caterpillar(tree1) and is_caterpillar(tree2):
         path = shortest_caterpillar_path(tree1,tree2)
         print("A shortest caterpillar path:")
-        #print the output in the command line in the same format at is was given in the txt files ([{},{},..])
-        for tree in path:
-            output = "["
-            for node in tree:
-                current_string = "{"
-                m = re.findall(r"(\d+)",str(node))
-                for i in m:
-                    current_string = current_string + str(i) + ","
-                current_string = current_string[:-1] + "}"
-                output = output + current_string + ","
-            output = output[:-1] + "]"
-            print(output)
+        for i in path:
+            print(i)
         print("Distance between the given trees:")
         print(len(path)-1)
     elif is_caterpillar(tree1) == False and is_caterpillar(tree2) == False:
