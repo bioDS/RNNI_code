f2b52492d4b57d25a35bdbcb462bbb5f7e2d44d2
Kieran Elmes <kelmes@caelus.nz>
2019-12-07 09:29:28 +1300

    don't malloc buffers inside loops

--- seidel.c
+++ seidel.c
@@ -43,6 +43,7 @@ void free_matrix(unsigned char **X, int n) {
  * Z doesn't need to be allocated, maybe X does?
  */
 dm seidel_recursive(dm A, int n, int depth) {
+	int num_threads = omp_get_max_threads();
 	if (depth > 5) {
 		dm D;
 		return D;
@@ -56,17 +57,32 @@ dm seidel_recursive(dm A, int n, int depth) {
 		printf("A:\n");
 		print(A, n);
 	}
+	printf("checking A\n");
+	uint32_t **row_A = malloc(num_threads*sizeof(uint32_t*));
+	uint32_t **row_B = malloc(num_threads*sizeof(uint32_t*));
+	unsigned char **row_B_buf = malloc(num_threads*sizeof(uint32_t*));
+	uint32_t **col_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+	uint32_t **col_B = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+	unsigned char **col_B_buf = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+	for (int i = 0; i < num_threads; i++) {
+		row_A[i] = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		row_B[i] = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		row_B_buf[i] = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		col_A[i] = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		col_B[i] = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		col_B_buf[i] = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+	}
+	#pragma omp parallel for shared(done)
 	for (int i = 0; i < n; i++) {
-		uint32_t *row_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-		p4ndec32(A.sa[i], n, row_A);
-		#pragma omp parallel for
+		int thread_id = omp_get_thread_num();
+		p4ndec32(A.sa[i], n, row_A[thread_id]);
 		for (int j = 0; j < n; j++) {
-			if (i != j && row_A[j] != 1) {
+			if (i != j && row_A[thread_id][j] != 1) {
 				done = 0;
 			}
 		}
-		free(row_A);
 	}
+	printf("done checking A\n");
 	// the array is all ones. We can just return it.
 	if (done) {
 		printf("floor\n");
@@ -83,82 +99,70 @@ dm seidel_recursive(dm A, int n, int depth) {
 		return D;
 	}
 
+	printf("calculating B\n");
 	dm B;
+	uint32_t *degree = malloc(n*sizeof(uint32_t));
 	B.sa = malloc(n*sizeof(unsigned char *));
 	B.sat = malloc(n*sizeof(unsigned char *));
-	uint32_t *degree = malloc(n*sizeof(uint32_t));
-	uint32_t *row_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-	uint32_t *row_B = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-	unsigned char *row_B_buf = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+	#pragma omp parallel for shared(degree)
 	for (int i = 0; i < n; i++) {
+		int thread_id = omp_get_thread_num();
 		uint32_t deg_i = 0;
-		p4ndec32(A.sa[i], n, row_A);
-		#pragma omp parallel for reduction(+: deg_i)
+		p4ndec32(A.sa[i], n, row_A[thread_id]);
 		for (int j = 0; j < n; j++) {
-			uint32_t *col_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-			p4ndec32(A.sat[j], n, col_A);
-			deg_i += row_A[j];
+			p4ndec32(A.sat[j], n, col_A[thread_id]);
+			deg_i += row_A[thread_id][j];
 			if (i == j) {
-				row_B[j] = 0;
+				row_B[thread_id][j] = 0;
 			} else {
-				row_B[j] = 0;
-				if (row_A[j] == 1) { // set the only other case where B[i*n+j] would be one while we're here.
-					row_B[j] = 1;
+				row_B[thread_id][j] = 0;
+				if (row_A[thread_id][j] == 1) { // set the only other case where B[i*n+j] would be one while we're here.
+					row_B[thread_id][j] = 1;
 				}
 				for (int k = 0; k < n; k++) {
-					if (row_A[k] > 0 && col_A[k] > 0) {
-						row_B[j] = 1;
+					if (row_A[thread_id][k] > 0 && col_A[thread_id][k] > 0) {
+						row_B[thread_id][j] = 1;
 						break;
 					}
 				}
 			}
-			free(col_A);
 		}
 		//memcpy(A.sa[i], row_A, n*sizeof(int)); //not changed
-		size_t row_b_size = p4nenc32(row_B, n, row_B_buf);
+		size_t row_b_size = p4nenc32(row_B[thread_id], n, row_B_buf[thread_id]);
 		B.sa[i] = malloc(row_b_size);
-		memcpy(B.sa[i], row_B_buf, row_b_size);
+		memcpy(B.sa[i], row_B_buf[thread_id], row_b_size);
 
 		degree[i] = deg_i;
 	}
-	free(row_A);
-	free(row_B);
-	free(row_B_buf);
-	uint32_t *col_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-	uint32_t *col_B = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-	unsigned char *col_B_buf = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+	printf("done calculating B\n");
+	printf("calculating Bt\n");
+	#pragma omp parallel for
 	for (int j = 0; j < n; j++) {
-		uint32_t deg_i = 0;
-		p4ndec32(A.sat[j], n, col_A);
-		#pragma omp parallel for
+		int thread_id = omp_get_thread_num();
+		p4ndec32(A.sat[j], n, col_A[thread_id]);
 		for (int i = 0; i < n; i++) {
-			uint32_t *row_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-			p4ndec32(A.sa[i], n, row_A);
-			deg_i += row_A[j];
+			p4ndec32(A.sa[i], n, row_A[thread_id]);
 			if (i == j) {
-				col_B[i] = 0;
+				col_B[thread_id][i] = 0;
 			} else {
-				col_B[i] = 0;
-				if (col_A[i] == 1) { // set the only other case where B[i*n+j] would be one while we're here.
-					col_B[i] = 1;
+				col_B[thread_id][i] = 0;
+				if (col_A[thread_id][i] == 1) { // set the only other case where B[i*n+j] would be one while we're here.
+					col_B[thread_id][i] = 1;
 				}
 				for (int k = 0; k < n; k++) {
-					if (row_A[k] > 0 && col_A[k] > 0) {
-						col_B[i] = 1;
+					if (row_A[thread_id][k] > 0 && col_A[thread_id][k] > 0) {
+						col_B[thread_id][i] = 1;
 						break;
 					}
 				}
 			}
-			free(row_A);
 		}
 		//memcpy(A.sa[i], row_A, n*sizeof(int)); //not changed
-		size_t col_b_size = p4nenc32(col_B, n, col_B_buf);
+		size_t col_b_size = p4nenc32(col_B[thread_id], n, col_B_buf[thread_id]);
 		B.sat[j] = malloc(col_b_size);
-		memcpy(B.sat[j], col_B_buf, col_b_size);
+		memcpy(B.sat[j], col_B_buf[thread_id], col_b_size);
 	}
-	free(col_B_buf);
-	free(col_A);
-	free(col_B);
+	printf("done calculating Bt\n");
 
 	if (verbose) {
 		printf("B:\n");
@@ -171,16 +175,17 @@ dm seidel_recursive(dm A, int n, int depth) {
 		print(T, n);
 	}
 
+	printf("calculating D\n");
 	free_matrix(B.sa, n);
 	free_matrix(B.sat, n);
 	dm D;
 	D.sa = malloc(n*sizeof(unsigned char*));
-	uint32_t *row_T = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-	uint32_t *row_D = malloc(BUF_SIZE(n)*sizeof(uint32_t));
-		unsigned char *row_D_buf = malloc(BUF_SIZE(n)*sizeof(int));
+	#pragma omp parallel for
 	for (int i = 0; i < n; i++) {
+		uint32_t *row_T = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		uint32_t *row_D = malloc(BUF_SIZE(n)*sizeof(uint32_t));
+		unsigned char *row_D_buf = malloc(BUF_SIZE(n)*sizeof(int));
 		p4ndec32(T.sa[i], n, row_T);
-		#pragma omp parallel for
 		for (int j = 0; j < n; j++) {
 			uint32_t *col_A = malloc(BUF_SIZE(n)*sizeof(uint32_t));
 			p4ndec32(A.sat[j], n, col_A);
@@ -201,10 +206,11 @@ dm seidel_recursive(dm A, int n, int depth) {
 		size_t row_d_size = p4nenc32(row_D, n, row_D_buf);
 		D.sa[i] = malloc(row_d_size);
 		memcpy(D.sa[i], row_D_buf, row_d_size);
+		free(row_D_buf);
+		free(row_T);
+		free(row_D);
 	}
-	free(row_D_buf);
-	free(row_T);
-	free(row_D);
+	printf("done calculating D\n");
 
 	if (verbose) {
 		printf("degree:\n");
@@ -218,6 +224,20 @@ dm seidel_recursive(dm A, int n, int depth) {
 		printf("D:\n");
 		print(D, n);
 	}
+	for (int i = 0; i < num_threads; i++) {
+		free(row_A[i]);
+		free(row_B[i]);
+		free(row_B_buf[i]);
+		free(col_A[i]);
+		free(col_B[i]);
+		free(col_B_buf[i]);
+	}
+	free(row_A);
+	free(row_B);
+	free(row_B_buf);
+	free(col_A);
+	free(col_B);
+	free(col_B_buf);
 	free_matrix(T.sa, n); // is A.sa
 	free(degree);
 
