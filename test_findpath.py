eb5feb0e6fe809408cad39bfe193713f5d9d7c08
Kieran Elmes <kelmes@caelus.nz>
2019-12-30 17:12:17 +1300

    Hopefully share memory with fork in test_findpath. Save distance matrices.

--- test_findpath.py
+++ test_findpath.py
@@ -7,20 +7,29 @@ from findpath import *
 from os.path import exists
 from itertools import repeat
 from multiprocessing import Pool
+from multiprocessing import sharedctypes
 import re
 
-def check_tree(tuple):
-    tree1 = tuple[0]
-    uRNNI = tuple[1]
-    D     = tuple[2]
-    l     = tuple[3]
+D = np.zeros((1,1))
+l = 0
+uRNNI = 0
+def check_tree(tree1):
+    global D
+    global l
+    global uRNNI
     for tree2 in uRNNI[1]:
-        if (len(findpath(string_to_tree(str(tree1)),string_to_tree(str(tree2))))-1 != D[l[uRNNI[1].index(tree1)]][l[uRNNI[1].index(tree2)]]):
+        findpath_len = len(findpath(string_to_tree(str(tree1)),string_to_tree(str(tree2)))) - 1
+        actual_len = D[l[uRNNI[1].index(tree1)]][l[uRNNI[1].index(tree2)]]
+        if (findpath_len != actual_len):
+            print("{} != {}, Failed on:".format(findpath_len, actual_len))
             print(tree1, tree2)
             return False
     return True
 
 def check_findpath(n):
+    global D
+    global l
+    global uRNNI
     # Check if FINDPATH computes exact distances by comparing these distances with exact ones (Seidel on RNNI graph)
     if exists('output/uRNNI_graphs/tree_numbers_%s_taxa.txt' %n):
         uRNNI = read_uRNNI_graph('output/uRNNI_graphs/tree_numbers_%s_taxa.txt' %n, 'output/uRNNI_graphs/uRNNI_edges_%s_taxa.txt' %n)
@@ -30,13 +39,27 @@ def check_findpath(n):
         uRNNI = uRNNI_graph_on_n_taxa(n)
         print("Done generating uRNNI graph on %s taxa on %s at %s" % (n, time.strftime("%a, %b %d %Y"), time.strftime("%H:%M:%S")))
 
-    FW = uRNNI[0].Seidel() #Seidel does not work for more than 7 taxa
-    D = FW[0]
-    l = FW[1]
+    if exists('output/distance_matrices/distance_matrix_%s_taxa.npy' %n):
+        S = np.load('output/distance_matrices/distance_matrix_%s_taxa.npy' %n)
+        l = S[1]
+    else:
+        print("Start generating distance matrix on %s taxa on %s at %s" % (n, time.strftime("%a, %b %d %Y"), time.strftime("%H:%M:%S")))
+        S = uRNNI[0].Seidel() #Seidel does not work for more than 7 taxa
+        #S.tofile('output/distance_matrices/distance_matrix_%s_taxa' %n)
+        np.save('output/distance_matrices/distance_matrix_%s_taxa.npy' %n, S)
+        l = S[1]
+    #FW[0][1][2] = 0
+    #tmp = np.ctypeslib.as_ctypes(S)
+    #D_shared = sharedctypes.RawArray(ctypes.c_int32, tmp)
+    #D = np.ctypeslib.as_array(tmp, shape=((n,n)))
+    D = S[0]
+
 
     print("Finding paths & checking their distances on %s at %s" % (time.strftime("%a, %b %d %Y"), time.strftime("%H:%M:%S")))
+    #for tree1 in uRNNI[1]:
+    #    check_tree(tree1)
     with Pool() as pool:
-        equal_distances = pool.map(check_tree, list(zip(uRNNI[1], repeat(uRNNI), repeat(D), repeat(l))))
+        equal_distances = pool.map(check_tree, uRNNI[1])
         all_correct = len(equal_distances) == sum(equal_distances)
         if (not all_correct):
             return False
