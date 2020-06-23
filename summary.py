4ad5f872d06a17dd8e7638b4b373d8248ea8a74a
Lars Berling <berlinglars96@gmail.com>
2020-06-23 08:15:08 +0200

    bug fix

--- summary.py
+++ summary.py
@@ -58,7 +58,7 @@ def get_closer(s,trees,distance):
     return(output)
 
 def centroid(trees,s):
-    # Centroid search starting from s
+    # extensive Centroid search starting from s
     # Initialization to run get_closer_function
     startdist = 0
     for i in trees:
@@ -66,20 +66,23 @@ def centroid(trees,s):
         startdist += path ** 2 
     closer = get_closer(s,trees,startdist)
     output = []
-    while bool(closer):
-        for i in range(0,len(closer)):
-            # for each tree that is closer
-            cur = get_closer(closer[i][0],trees,closer[i][1])    
-            if bool(cur):
-                # there are trees that give a smaller distance = cur
-                # we need to add each tree to close, i.e. check if there is anything in their neighbourhood
-                # which gives better distance
-                for j in cur:
-                    closer.append(j)
-            else:
-                # No tree in one neighbourhood of i is getting any better distance
-                # --> i is a local optimum and gets put into the output and deleted from things to work on
-                output.append(closer[i])
-            closer.pop(i)
+    if not closer:
+        output.append([expand(s),startdist])
+    else:    
+        while bool(closer):
+            for i in range(0,len(closer)):
+                # for each tree that is closer
+                cur = get_closer(closer[i][0],trees,closer[i][1])    
+                if bool(cur):
+                    # there are trees that give a smaller distance = cur
+                    # we need to add each tree to close, i.e. check if there is anything in their neighbourhood
+                    # which gives better distance
+                    for j in cur:
+                        closer.append(j)
+                else:
+                    # No tree in one neighbourhood of i is getting any better distance
+                    # --> i is a local optimum and gets put into the output and deleted from things to work on
+                    output.append(closer[i])
+                closer.pop(i)
     return(output)
 
